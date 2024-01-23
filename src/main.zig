22e6f9efdfc56d4c4c7398890a11191510d72117
ChobbyCode <jacobhadfield99@gmail.com>
2024-01-23 19:57:06 +0000

    It works!!!!!

--- src/main.zig
+++ src/main.zig
@@ -2,5 +2,13 @@ const std = @import("std");
 const list = @import("Methods/list.zig");
 
 pub fn main() !void {
-    list.ListMethods();
+    // Get the arguments past into the application
+    var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
+    const gpa = general_purpose_allocator.allocator();
+    const args = try std.process.argsAlloc(gpa);
+    defer std.process.argsFree(gpa, args);
+
+    if (args.len == 1) {
+        list.ListMethods();
+    }
 }
