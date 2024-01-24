d3c6b91973ba17e7634a3a97c709fb336e440248
ChobbyCode <jacobhadfield99@gmail.com>
2024-01-24 16:04:34 +0000

    I decide to change what this zig is gonna do

--- src/main.zig
+++ src/main.zig
@@ -1,33 +1,3 @@
 const std = @import("std");
-const print = std.debug.print();
-const list = @import("Methods/list.zig");
 
-// Methods Stuff
-const Installer = @import("install.zig");
-
-pub fn main() !void {
-    // Get the arguments past into the application
-    var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
-    const gpa = general_purpose_allocator.allocator();
-    const args = try std.process.argsAlloc(gpa);
-    defer std.process.argsFree(gpa, args);
-
-    if (args.len == 1) {
-        list.ListMethods();
-    } else {
-        processInput(args[1]);
-    }
-}
-
-fn processInput(i: []const u8) !void {
-    // Ugly if statements because I don't think there's switch statements
-    if (std.mem.eql(i, "install")) {
-        Installer.Install();
-    } else {
-        print("Invalid 'chad' input");
-    }
-}
-
-fn printTerminal(i: []const u8) !void {
-    print(i ++ "\n", .{});
-}
+pub fn main() !void {}
