b2ca4a2c36d53d1243aeabbb83724c193db3e302
ChobbyCode <jacobhadfield99@gmail.com>
2024-01-28 10:11:27 +0000

    Parser + Parser Messages

---
+++ src/TerminalChad/Input/InputMessages.cs
@@ -0,0 +1,17 @@
+﻿
+namespace TerminalChad.CLI.Input;
+
+internal class InputMessages
+{
+    public static void PrintBasic()
+    {
+        Console.WriteLine("Copyright (c) ChobbyCode 2024, All Rights Reserved, MIT License");
+        Console.WriteLine("Terminal Chad is a Terminal extension to make is look better quicker and more easily.");
+    }
+
+    public static void PrintVersion()
+    {
+        Console.WriteLine("Copyright (c) ChobbyCode 2024, All Rights Reserved, MIT License");
+        Console.WriteLine("TerminalChad v0.1.0");
+    }
+}
--- src/TerminalChad/Program.cs
+++ src/TerminalChad/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using TerminalChad.CLI.Input;
+
+namespace TerminalChad.CLI;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        InputParser parser = new InputParser();
+        parser.ParseInput(args);
+    }
+}
