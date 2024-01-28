bdc0c9dbbaad120abc22f1a8d13384f6a088f18e
ChobbyCode <jacobhadfield99@gmail.com>
2024-01-28 10:02:00 +0000

    GitIgnore + CSharp

--- install.ps1
+++
@@ -1,8 +0,0 @@
-$RunDirectory = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
-
-winget install --id=Neovim.Neovim  -e
-git clone https://github.com/NvChad/NvChad %USERPROFILE%\AppData\Local\nvim --depth 1 && nvim
-winget install zig.zig
-
-Write-Host "Please restart your shell for certain changes to take place"
-Read-Host;
