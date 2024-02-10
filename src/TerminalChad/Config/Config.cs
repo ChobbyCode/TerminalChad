2b835be567d01e2beed325fdcaccf24127840b40
ChobbyCode <jacobhadfield99@gmail.com>
2024-02-10 21:51:51 +0000

    (Implementation Partial) - Added Yaml Support For Config Files
    Added yaml support for config files because it is easier to read as an unexperienced user, and json is confusing when big.

---
+++ src/TerminalChad/Config/Config.cs
@@ -0,0 +1,41 @@
+﻿
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace TerminalChad.Config;
+
+// Before anyone asks why we use yaml. We are using it as for unexperienced people can understand it a lot easier, because json can be difficult to read for config files which
+// are edited by the user
+
+public class Config
+{
+    public string StartDirectory { get; set; } = "~";
+    public List<String> StartApplications { get; set; } = new List<String>();
+    public bool forceLSPs { get; set; }
+
+    public void WriteConfig()
+    {
+        var serializer = new SerializerBuilder()
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .Build();
+        var yaml = serializer.Serialize(this);
+        if (!File.Exists($"{GlobalVariables.RoamingDirectory}config.yml"))
+        {
+            StreamWriter sz = new StreamWriter($"{GlobalVariables.RoamingDirectory}config.yml");
+            try
+            {
+                sz.Write("");
+            }
+            catch { }
+            finally { sz.Close(); }
+        }
+        StreamWriter sw = new StreamWriter($"{GlobalVariables.RoamingDirectory}config.yml");
+        try
+        {
+            sw.Write( yaml );
+        }catch { }
+        finally { sw.Close(); }
+
+        System.Console.WriteLine(yaml);
+    }
+}
--- src/TerminalChad/Setup/DirectorySetup.cs
+++ src/TerminalChad/Setup/DirectorySetup.cs
@@ -6,12 +6,14 @@ public class DirectorySetup
     private string _terminalChadFolder = $"C:/users/{Environment.UserName}/appdata/roaming/TerminalChad/";
     private string _themeFolder = $"C:/users/{Environment.UserName}/appdata/roaming/TerminalChad/Themes/";
     private string _activeFolder = $"C:/users/{Environment.UserName}/appdata/roaming/TerminalChad/.active/";
+    private string _languageServers = $"C:/users/{Environment.UserName}/appdata/roaming/TerminalChad/Microsoft.LanguageServerProtocols";
 
     public void CreateDirectories()
     {
         CreateDirectory(_terminalChadFolder);
         CreateDirectory(_themeFolder);
         CreateDirectory(_activeFolder);
+        CreateDirectory(_languageServers);
     }
 
     private void CreateDirectory(string path)
